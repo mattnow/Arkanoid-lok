878f523f8095b30dd85ccd3b458d6b3cbc833c35
mattnow <mateusz.kuba@gazeta.pl>
2019-06-16 16:30:17 +0200

    Add ball and basic animations

--- Arkanoid/Game.h
+++ Arkanoid/Game.h
@@ -34,12 +34,18 @@ namespace Arkanoid {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::PictureBox^ ball;
+	protected:
+
+	protected:
+	private: System::Windows::Forms::Timer^ timer;
+	private: System::ComponentModel::IContainer^ components;
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,24 +54,64 @@ namespace Arkanoid {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Game::typeid));
+			this->ball = (gcnew System::Windows::Forms::PictureBox());
+			this->timer = (gcnew System::Windows::Forms::Timer(this->components));
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ball))->BeginInit();
 			this->SuspendLayout();
+			// 
+			// ball
+			// 
+			this->ball->BackColor = System::Drawing::Color::Transparent;
+			this->ball->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"ball.Image")));
+			this->ball->Location = System::Drawing::Point(570, 276);
+			this->ball->Name = L"ball";
+			this->ball->Size = System::Drawing::Size(27, 27);
+			this->ball->SizeMode = System::Windows::Forms::PictureBoxSizeMode::AutoSize;
+			this->ball->TabIndex = 0;
+			this->ball->TabStop = false;
+			// 
+			// timer
+			// 
+			this->timer->Enabled = true;
+			this->timer->Interval = 20;
+			this->timer->Tick += gcnew System::EventHandler(this, &Game::Timer_Tick);
 			// 
 			// Game
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::Highlight;
 			this->ClientSize = System::Drawing::Size(1264, 681);
+			this->Controls->Add(this->ball);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
 			this->MaximizeBox = false;
 			this->Name = L"Game";
 			this->Text = L"Arkanoid";
 			this->FormClosing += gcnew System::Windows::Forms::FormClosingEventHandler(this, &Game::Game_FormClosing);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->ball))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
+		int x = 10, y = 10;
 #pragma endregion
 	private: System::Void Game_FormClosing(System::Object^ sender, System::Windows::Forms::FormClosingEventArgs^ e) {
 		Application::Exit(); //zamyka program przy wcisniêciu "zamknij" (x) na tym oknie
+	}
+	private: System::Void Timer_Tick(System::Object^ sender, System::EventArgs^ e) {
+		
+		ball->Left += x;
+		ball->Top += y;
+		if (ball->Left >= 1250 || ball->Left <=0 )
+		{
+			x = -x;
+		}
+		if (ball->Top >= 690 || ball->Top <= 0)
+		{
+			y = -y;
+		}
 	}
 	};
 }
